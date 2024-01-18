e5709da4d96115348de01accda8a6ab2dec2b1e6
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2024-01-17 23:04:52 -0500

    Week 2, class 2

---
+++ server/routes/employee.js
@@ -0,0 +1,40 @@
+"use strict"
+
+// Require statements
+const express = require('express');
+const router = express.Router();
+const { mongo } = require('../utils/mongo');
+
+router.get("/:empId",  (req, res, next) => {
+  try{
+    let { empId } = req.params;
+    empId = parseInt(empId, 10);
+
+    if (isNaN(empId)) {
+      const err = new Error('Employee ID must be a number');
+      err.status = 400;
+      console.log(err);
+      next(err);
+      return;
+    }
+
+    mongo(async db => {
+      const employee = await db.collection('employees').findOne({ empId });
+
+      if (!employee) {
+        const err = new Error('Employee not found');
+        err.status = 404;
+        console.log("err", err);
+        next(err);
+        return;
+      }
+      res.send(employee);
+    });
+  }
+  catch(err) {
+  console.error("Error: ". err);
+  next(err);
+  }
+})
+
+module.exports = router;
---
+++ server/utils/mongo.js
@@ -0,0 +1,35 @@
+"use strict"
+
+// Import Mongo
+const { MongoClient } = require('mongodb');
+
+// Define connection string
+const MONGO_URL = "mongodb+srv://nodebucket_user:s3cret@bellevueuniversity.w2mknhu.mongodb.net/nodebucket?retryWrites=true&w=majority"
+
+const mongo = async (operations, next) => {
+
+  try {
+    console.log('Connecting to database...');
+
+    const client = await MongoClient.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    const db = client.db('nodebucket');
+    console.log('Connected to database');
+
+    await operations(db);
+    console.log('Operation was successful');
+
+    client.close();
+    console.log('Connection closed');
+
+  } catch (err) {
+    const error = new Error('Application Error', err);
+    error.status = 500;
+    console.log("Error connecting to the database:", err);
+  }
+};
+
+module.exports = { mongo };
